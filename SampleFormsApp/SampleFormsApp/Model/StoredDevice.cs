2047311e522ebf32755f040b255c0fc4c4935021
Benjamin Goltz <benjamin@socketmobile.com>
2023-11-17 14:56:21 +0100

    compatible nuget sdk v.1.4.8.28

---
+++ SampleFormsApp/SampleFormsApp/Model/StoredDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocketMobile.Capture;
+
+namespace SampleFormsApp.Model
+{
+    public class StoredDevice
+    {
+        public CaptureHelperDevice DeviceObj {  get; set; }
+        public string DeviceName { get; set; }
+    }
+}
