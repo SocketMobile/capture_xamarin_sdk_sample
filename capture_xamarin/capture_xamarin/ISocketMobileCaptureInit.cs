7aafed93113664f4820675bcc457489a07ce5a0c
Benjamin Goltz <benjamin@socketmobile.com>
2023-11-17 14:56:54 +0100

    rename folders to capture_xamarin_sdk_sample

---
+++ capture_xamarin/capture_xamarin/ISocketMobileCaptureInit.cs
@@ -0,0 +1,19 @@
+﻿using SocketMobile.Capture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace capture_xamarin_sdk_sample
+{
+    public interface ISocketMobileCaptureInit
+    {
+        MainPage MainPage { get; set; }
+        void StartCaptureCore(string appId, string developerId, string appKey);
+
+        void DeviceList_SelectedIndexChanged(Picker deviceList);
+        void DeviceList_Focused(Picker deviceList);
+        void GetSocketCamStatusInit();
+        void Switch_SocketCamStatus();
+    }
+}
---
+++ capture_xamarin/capture_xamarin/Model/StoredDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocketMobile.Capture;
+
+namespace capture_xamarin_sdk_sample.Model
+{
+    public class StoredDevice
+    {
+        public CaptureHelperDevice DeviceObj {  get; set; }
+        public string DeviceName { get; set; }
+    }
+}
